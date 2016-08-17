7d6d0c8a68509249b20dfc537572d9348fd0115d
Novikov Mikhail <star4beam@gmail.com>
2016-08-17 18:00:47 +0300

    module1 task2

--- build/js/check.js
+++ build/js/check.js
@@ -2,24 +2,22 @@ function getMessage(a, b) {
   if (typeof a === 'boolean') {
     if (a) {
       return 'Я попал в ' + b;
-    } else { 
+    } else {
       return "Я никуда не попал";
     };
   } else if (typeof a === 'number') {
-      return 'Я прыгнул на ' + a * 100 + ' сантиметров';
+    return 'Я прыгнул на ' + a * 100 + ' сантиметров';
   } else if (Array.isArray(a) && !Array.isArray(b)) {
-      var numberOfSteps = for (var i = 0; i < a.length; i++) {
-        var total = 0;
-        total += a[i];
-        return total;
+    var numberOfSteps = 0;
+    for (var i = 0; i < a.length; i++) {
+        numberOfSteps += a[i];
       };
-      return 'Я прошёл ' + numberOfSteps + ' шагов';
+    return 'Я прошёл ' + numberOfSteps + ' шагов';
   } else if (Array.isArray(a) && Array.isArray(b)) {
-      var distancePath = for (var i = 0; i < a.length; i++) {
-        var totalProd = 0;
-        totalProd += (a[i] * b[i]);
-        return totalProd;
+    var distancePath = 0; 
+    for (var i = 0; i < a.length; i++) {
+        distancePath += (a[i] * b[i]);
       };
-      return 'Я прошёл ' + distancePath + ' метров';
+    return 'Я прошёл ' + distancePath + ' метров';
   };
 };
