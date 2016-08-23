afdcc34cea0b87d00e80611630901dd876d634e5
Novikov Mikhail <star4beam@gmail.com>
2016-08-24 00:07:43 +0300

    JSONP getCallback function fix

--- src/reviews.js
+++ src/reviews.js
@@ -1,32 +1,31 @@
+'use strict';
 // JSONPCallback is callback name
 var CALLBACK_URL = 'http://localhost:1506/api/reviews?callback=JSONPCallback';
-
-
 var reviews = [];
 
-// Saves received by JSONPCallback function data 
+// Saves received by JSONPCallback function data
 function saveReviewsData(data) {
   reviews = data;
   console.log(reviews);
 }
 
 
-function getReviews(CALLBACK_URL, saveReviewsData) {
+function getCallback(url, doSomething) {
 
 /*Describing JSONPCallback function
 JSONPCallback  - same as callback name */
   window.JSONPCallback = function(data) {
-    saveReviewsData(data);
-  }
+    doSomething(data);
+  };
 
 // Describing our new script
   var scriptEl = document.createElement('script');
 
-// Launches HTTP request with CALLBACK_URL
-  scriptEl.src = CALLBACK_URL;
+// Launches HTTP request with url
+  scriptEl.src = url;
 
 // Creating and running our new script with JSONPCallback function
   document.body.appendChild(scriptEl);
 }
 
-getReviews(CALLBACK_URL, saveReviewsData);
+getCallback(CALLBACK_URL, saveReviewsData);
